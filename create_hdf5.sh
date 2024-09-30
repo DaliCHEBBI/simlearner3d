6a4412383f7ec3efac34980390a154393b6daae7
“Mohamed <med.chebbi.mac@gmail.com>
2024-09-30 10:23:06 +0200

    ignore some files

---
+++ create_hdf5.sh
@@ -0,0 +1,4 @@
+python run.py task.task_name=create_hdf5 \
+datamodule.data_dir="/media/mohamedali/PAT443/DATASET_STEREO/Stereo" \
+datamodule.split_csv_path="/media/mohamedali/PAT443/DATASET_STEREO/Stereo/split.csv" \
+datamodule.hdf5_file_path="/media/mohamedali/PAT443/DATASET_STEREO/Stereo/eurosdr.hdf5"
---
+++ train_eurosdr.sh
@@ -0,0 +1,3 @@
+python run.py task.task_name="fit" \
+datamodule.hdf5_file_path="/media/mohamedali/PAT443/DATASET_STEREO/Stereo/eurosdr.hdf5" \
+experiment="MSAFFDebug"
