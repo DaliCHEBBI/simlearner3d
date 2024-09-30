7cd7d6ca231275006c2c181c3df496b20f02fcfd
DaliCHEBBI <mohamed-ali.chebbi@ign.fr>
2024-09-30 11:01:54 +0200

    add msaff inference consideration

--- simlearner3d/models/modules/msaff.py
+++ simlearner3d/models/modules/msaff.py
@@ -223,3 +223,91 @@ class MSNetPatch(torch.nn.Module):
 		# Pass to last conv block
 		x_all=self.common(x_all)
 		return x_all
+	
+class MSNETInferenceGatedAttention(torch.nn.Module):
+	def __init__(self,Inplanes):
+		super(MSNETInferenceGatedAttention, self).__init__()
+		self.inplanes = Inplanes
+		#self.params=param
+		self.firstconv = nn.Sequential(convbn(1, 32, 3, 1, 1, 1),
+										nn.ReLU(inplace=True),
+										convbn(32, 32, 3, 1, 1, 1),
+										nn.ReLU(inplace=True),
+										convbn(32, 32, 3, 1, 1, 1),
+										nn.ReLU(inplace=True))
+		
+		self.layer1 = self._make_layer(BasicBlock, 64, 3, 1,1,1)
+		self.layer2 = self._make_layer(BasicBlock, 64, 3, 1,1,1) 
+		self.layer3 = self._make_layer(BasicBlock, 64, 3, 1,1,1)
+		self.layer4 = self._make_layer(BasicBlock, 64, 3, 1,1,1)
+
+		# Multi-Scale fusion attention modules 
+		self.MultiScaleFeatureFuser3_4=MSCAM(64,4)
+		self.MultiScaleFeatureFuser2_3_4=MSCAM(64,4)
+		self.MultiScaleFeatureFuser1_2_3_4=MSCAM(64,4)
+		
+		# common network to compute the last features 
+		self.common=nn.Sequential(convbn(64,64,3, 1, 1, 1),
+									nn.ReLU(inplace=True),
+									convbn(64,64,3, 1, 1, 1),
+									nn.ReLU(inplace=True),
+									nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, dilation = 1, bias=False))
+
+	def _make_layer(self, block, planes, blocks, stride, pad, dilation):
+		downsample = None
+		if stride != 1 or self.inplanes != planes * block.expansion:
+			downsample = nn.Sequential(
+					nn.Conv2d(self.inplanes, planes * block.expansion,
+					kernel_size=1, stride=stride, bias=False),
+					nn.BatchNorm2d(planes * block.expansion))
+		layers = []
+		layers.append(block(self.inplanes, planes, stride, downsample, pad, dilation))
+		#self.inplanes = planes * block.expansion
+		for i in range(1, blocks):
+			layers.append(block(planes, planes,1,None,pad,dilation))
+		return nn.Sequential(*layers)
+	# msnet attention forward 
+	def forward(self,x):
+		if x.size()[-2] % 8 != 0:
+			times = x.size()[-2]//8   
+			top_pad = (times+1)*8 - x.size()[-2]
+		else:
+			top_pad = 0
+		if x.size()[-1] % 8 != 0:
+			times = x.size()[-1]//8
+			right_pad = (times+1)*8-x.size()[-1] 
+		else:
+			right_pad = 0    
+		x = F.pad(x,(0,right_pad, top_pad,0))
+		#print("padded size ",x.shape)
+		# interpolate input images
+		x_by2 = F.interpolate(x, (x.size()[2]//2,x.size()[3]//2),mode='bilinear')
+		x_by4 = F.interpolate(x, (x.size()[2]//4,x.size()[3]//4),mode='bilinear')  
+		x_by8 = F.interpolate(x, (x.size()[2]//8,x.size()[3]//8),mode='bilinear')  
+		x1=self.firstconv(x)
+		x2=self.firstconv(x_by2)
+		x3=self.firstconv(x_by4)
+		x4=self.firstconv(x_by8)
+		#print(x1.shape,x2.shape,x3.shape,x4.shape)
+		x1=self.layer1(x1)
+		x2=self.layer2(x2)
+		x3=self.layer3(x3)
+		x4=self.layer4(x4)
+		#print(x1.shape,x2.shape,x3.shape,x4.shape)
+		x4=F.interpolate(x4, (x3.size()[2],x3.size()[3]),mode='bilinear')
+		x_3_4=self.MultiScaleFeatureFuser3_4(x3,x4)
+		x_3_4=F.interpolate(x_3_4, (x2.size()[2],x2.size()[3]),mode='bilinear')
+		x_2_3_4=self.MultiScaleFeatureFuser2_3_4(x2,x_3_4)
+		x_2_3_4=F.interpolate(x_2_3_4, (x1.size()[2],x1.size()[3]),mode='bilinear')
+		x_all=self.MultiScaleFeatureFuser1_2_3_4(x1,x_2_3_4)
+		# Pass to last conv block
+		x_all=self.common(x_all)
+		if top_pad !=0 and right_pad != 0:
+			out = x_all[:,:,top_pad:,:-right_pad]
+		elif top_pad ==0 and right_pad != 0:
+			out = x_all[:,:,:,:-right_pad]
+		elif top_pad !=0 and right_pad == 0:
+			out = x_all[:,:,top_pad:,:]
+		else:
+			out = x_all
+		return out
